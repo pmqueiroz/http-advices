6a6a5453c8585f8f6041f21d3b2f2aa48aec61a3
pmqueiroz <pedroqueiroz593@gmail.com>
2021-06-08 16:58:27 -0300

    feat: return users on /users

--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type User struct {
+	Email string `json:"Email"`
+	Password string `json:"Password"`
+	Bio string `json:"Bio"`
+}
+
+type Users []User
+
 func baseUrl(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to my Go api")
 }
 
+func allUsers(w http.ResponseWriter, r *http.Request) {
+	users := Users{
+		User{Email: "cleiton@mail.go", Password: "myfirstgoapi123", Bio: "Hello, im a test user for this api"},
+	}
+	
+	fmt.Println(w, "All users")
+	json.NewEncoder(w).Encode(users)
+}
 
 func handeRequests() {
 	http.HandleFunc("/", baseUrl)
+	http.HandleFunc("/users", allUsers)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
